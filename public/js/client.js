6fd2f75fb7e71b64c9d96657b67de633b925d3d3
lillianritchie <lr2527@nyu.edu>
2020-04-30 16:51:46 -0400

    pattern selector working

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,41 @@
 //event listener for the DOM being loaded
 window.addEventListener("DOMContentLoaded", () => {
-
+    console.log('dom is loaded!')
     //look for the form where we set the astrological sign
-    const signForm = document.getElementById("sign-form");
+    const $signForm = document.getElementById("sign-form");
+
+    //console.log(signForm.class);
 
     //create an event parameter
-    signForm.onsubmit = (event) => {
-        //look up the value that was submitted and save it to a const
-        const signInput = event.target.value;
-        console.log(signInput);
+    $signForm.addEventListener('change', async evt => {
+        //prevent the default reload
+
+        console.log('changed');
+        event.preventDefault();
+        console.log(evt.target.value);
+        await createPattern(evt.target.value);
+
+    })
+
+});
+
+function createPattern(input) {
+    console.log("creating a pattern for " + input);
+
+    let $patternContainer1 = document.getElementById("pattern-1");
+    let $patternContainer2 = document.getElementById("pattern-2");
+
+    if (input == "null") {
+        patternContainer1.innerHTML = `<h3 class="content-heading__sub">Don't Forget to select your constellation pattern!</h3>`;
+        $patternContainer2.innerHTML = `<h3 class="content-heading__sub">Don't Forget to select your constellation pattern!</h3>`;
+    } else {
+
+    $patternContainer1.innerHTML = `<img class="pattern-image" src="../../assets/${input}-layer-01.png"></img>`;
+    $patternContainer2.innerHTML = `<img class="pattern-image" src="../../assets/${input}-layer-02.png"></img>`;
     }
 
 
-});
+
+
+
+}
